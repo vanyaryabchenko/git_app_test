5fdc5c77195b645c418e83c78ef580ceac04b213
Ivan.Ryabchenko <vanya.ryabchenko@gmail.com>
2016-12-10 23:52:03 +0200

    Add a cycle

--- src/com/java/git/test/Main.java
+++ src/com/java/git/test/Main.java
@@ -2,5 +2,8 @@ package com.java.git.test;
 public class Main{
    public static void main(String[] args) {
     System.out.println("Hello Git");
+    for(int i=0; i<=10; i++){
+      System.out.println(i);
+    }
   }
 }
